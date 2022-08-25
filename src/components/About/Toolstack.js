c8c782bba17a8af1780d054d6b83099ea975dc96
Anik Ronjon <anik35-497@diu.edu.bd>
2022-08-26 04:39:14 +0600

    My portfolio

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { DiAws } from "react-icons/di";
+import { FaAmazon, FaAws, FaDigitalOcean } from "react-icons/fa";
+import {
+  SiLinux,
+  SiVisualstudiocode,
+  SiPostman,
+  SiHeroku,
+  SiVercel,
+  SiPycharm,
+  SiDocker,
+  SiMicrosoftazure,
+} from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiLinux />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPycharm />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiDocker />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHeroku />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaAws />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaDigitalOcean />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMicrosoftazure />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
